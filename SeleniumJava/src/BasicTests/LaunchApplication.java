24669a9b42575010a00cf7a7102028c78259289a
Tssumanth18 <tssumanth18@gmail.com>
2020-03-05 23:45:11 +0530

    Launch browser changes

--- SeleniumJava/src/BasicTests/LaunchApplication.java
+++ SeleniumJava/src/BasicTests/LaunchApplication.java
@@ -3,32 +3,42 @@ package BasicTests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LaunchApplication {
 
 	static WebDriver driver;
 	public static void main(String[] args) 
 	{
+		launchBrowser("FIREFOX");
+		launchApplication("https://www.linkedin.com/");
+		quitApplication();
+		System.out.println("Execution Completed Firefox");
+		launchBrowser("CHROME");
+		launchApplication("https://www.linkedin.com/");
+		quitApplication();
+		System.out.println("Execution Completed CHROME");
 		launchBrowser("HEADLESSCHROME");
 		launchApplication("https://www.linkedin.com/");
 		quitApplication();
-		System.out.println("Execution Completed");
+		System.out.println("Execution Completed HEADLESSCHROME");
 	}
 	
 	public static void launchBrowser(String BrowserName)
 	{
 		if(BrowserName.toUpperCase().equals("FIREFOX"))
 		{
-			
+			System.setProperty("webdriver.gecko.driver", "D:\\Selenium Java\\chromedriver\\geckodriver.exe");
+			driver=new FirefoxDriver();
 		}
 		else if(BrowserName.toUpperCase().equals("CHROME"))
 		{
-			System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "D:\\Selenium Java\\chromedriver\\chromedriver.exe");
 			driver=new ChromeDriver();
 		}
 		else if(BrowserName.toUpperCase().equals("HEADLESSCHROME"))
 		{
-			System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "D:\\Selenium Java\\chromedriver\\chromedriver.exe");
 			ChromeOptions chromeOptions = new ChromeOptions();
 			chromeOptions.addArguments("--window-size=1200x600");
 		    chromeOptions.addArguments("--headless");
@@ -40,6 +50,13 @@ public class LaunchApplication {
 	public static void launchApplication(String ApplicationURL)
 	{
 		driver.get(ApplicationURL);
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(driver.getTitle());
 	}
 	
 	public static void quitApplication()
